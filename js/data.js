1cf66ef3f1d9753761c4f6bbb081696616302b56
William Funk <contact@WilliamRADFunk.com>
2015-09-19 22:18:44 -0400

    Added sound files, and loaded them.

--- js/data.js
+++ js/data.js
@@ -1,24 +1,42 @@
-var camera;
-var camera1;
-var camera2;
-var camera3;
-var cameraSelected = 1;
+var background_gunfire_01,
+	background_gunfire_02,
+	background_gunfire_03,
+	cannon_fire_01,
+	cannon_fire_02,
+	cannon_fire_03,
+	cannon_fire_04,
+	collision_explosion_01,
+	collision_explosion_02,
+	collision_explosion_03,
+	collision_explosion_04,
+	theme_beach_01,
+	theme_beach_02,
+	theme_rainforest_01,
+	theme_thunder_01,
+	theme_thunder_02,
+	theme_thunder_03,
+	theme_thunder_04,
+	theme_thunder_05,
+	theme_thunder_06;
+var camera,
+	camera1,
+	camera2,
+	camera3,
+	cameraSelected = 1;
 var scene;
-var fog;
 var renderer;
+var keyboard;
 var orbControls;
-var groundplane;
+var theme = 0;
 var cannon;
 var ball;
-var castleTemplate1 = ["5", "000","010","020","030","040","050","100","200","300","400","500","150","250","350","450","550","510","520","530","540"];
 var ballList = [];
-var castle = [];
+var ballChanged = false;
 var caliber = 0;
+var castleTemplate1 = ["5", "000","010","020","030","040","050","100","200","300","400","500","150","250","350","450","550","510","520","530","540"];
+var castle = [];
 var updateCounter = 0;
 var lastFired = 0;
-var ballChanged = false;
-var keyboard;
-var balllaunched = false;
 
 var brickTexture;
 var ball1Texture;
